f7cf949b39ba2216106ff1e7a8ad1a410b358c86
Tarek Abdo <tarekk.abdo22@gmail.com>
2024-12-05 22:36:57 +0200

    start organizing

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <CategoriesScreen />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
