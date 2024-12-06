42834d78ea29ad93f89eb453eddd81067255e6f1
Tarek Abdo <tarekk.abdo22@gmail.com>
2024-12-06 14:12:23 +0200

    displaying Meals

--- screens/MealsOverViewScreen.js
+++ screens/MealsOverViewScreen.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { MEALS } from "../data/dummy-data";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import MealItem from "../components/MealItem";
 
 const MealsOverViewScreen = ({ route }) => {
   // this route.params will give us the object the we defined navigation.navigate("MealsOverview"
   const catId = route.params.categoryId;
+  console.log(catId, "CATID");
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0; // this means we have a match
+  });
+  console.log(displayedMeals);
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
 
   return (
     <View style={styles.contianer}>
-      <Text>Meals Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        renderItem={renderMealItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 };
